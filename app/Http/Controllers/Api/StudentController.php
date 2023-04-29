923f707c0c33c50696eb1fdc950eb1d57b7089b4
GabrielMusbah <contadomusbah@gmail.com>
2023-04-29 18:04:08 -0300

    1.2 - Criacao do controller do student

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StudentFormRequest;
 use App\Models\Student;
 use Illuminate\Http\Request;
 
@@ -17,5 +18,39 @@ class StudentController extends Controller
         }
 
         return $query->get();
+    }
+
+    public function store(StudentFormRequest $request)
+    {
+        $student = Student::create($request->all());
+
+        return response()->json($student, 201);
+    }
+
+    public function show(int $student)
+    {
+        $student = Student::find($student);
+
+        if ($student === null) {
+            return response()->json(['message' => 'Student not found'], 404);
+        }
+
+        return $student;
+    }
+
+    public function update(Student $student, StudentFormRequest $request)
+    {
+        $student->fill($request->all());
+
+        $student->save();
+
+        return $student;
+    }
+
+    public function destroy(int $student)
+    {
+        Student::destroy($student);
+
+        return response()->noContent();
     }
 }
